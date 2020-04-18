4f9b06849b7f8f5c894b10bf946111c6dad06f40
Mh-Tahir <58627780+Mh-Tahir@users.noreply.github.com>
2020-04-18 23:14:44 +0300

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -1,3 +1,65 @@
+#Hangman game with a random word
+import random
+from random_word import RandomWords
+
+
+print('H A N G M A N\nGuess a word. You can make 10 mistakes.')
+abc = 'qwertyuiopasdfghjklzxcvbnm'
+
+
+def game():
+    r = RandomWords()
+    word = r.get_random_word(hasDictionaryDef="true")
+    s = set()
+    typed = set()
+    lifes = 10
+    while lifes > 0:
+        print()
+        for k in word:
+            if k in s:
+                print(k, end='')
+            else:
+                print('-', end='')
+        print()
+        print('Input a letter: ', end='')
+        n = input()
+        if n in typed:
+            print('You already typed this letter')
+        elif len(n) > 1:
+            print('You should print a single letter')
+        elif n not in abc:
+            print('It is not an ASCII lowercase letter')
+        elif n not in word:
+            print('No such letter in the word')
+            lifes -= 1
+            typed.add(n)
+        else:
+            s.add(n)
+            typed.add(n)
+        if set(word) == s:
+            print('You guessed the word!\nYou survived!\n')
+            menu()
+            break
+    else:
+        print(f'You are hanged!\nThe word was {word}\n')
+        menu()
+
+
+def menu():
+    x = ''
+    while x not in ['play', 'exit']:
+        print('Type "play" to play the game, "exit" to quit: ', end='')
+        x = input()
+        if x == 'exit':
+            break
+        elif x == 'play':
+            game()
+
+
+menu()
+
+
+#Hangman game with 4 languages
 import random
 
 
